a5468dbf91d249833f6978ca59070558b85ed81a
surisaday377 <up230377@alumnos.upa.edu.mx>
2026-02-22 23:23:08 -0600

    tarea4

--- src/Helper.elm
+++ src/Helper.elm
@@ -1,11 +1,57 @@
 module Helper exposing (..)
 
-import Html
-import Html.Attributes
+import Char
+import Html exposing (Html, a, div, h1, h2, h3, h4, h5, h6, text)
+import Html.Attributes exposing (href)
 
 
 
--- Puedes usar una definición como la siguiente para probar y visualizar tus resultados, solo debes definir "headers" y "hyperlink"
---main : Html.Html msg
---main =
---    Html.div [] [ headers "Titulos", hyperlink "https://upa.edu.mx" "My School" ]
+-- 1️⃣ joinWords
+
+
+joinWords : String -> String -> String
+joinWords word1 word2 =
+    word1 ++ word2
+
+
+
+-- 2️⃣ isUpperChars
+
+
+isUpperChars : List Char -> List Bool
+isUpperChars list =
+    List.map Char.isUpper list
+
+
+
+-- 3️⃣ evalChars (función generalizada)
+
+
+evalChars : List Char -> (Char -> Bool) -> List Bool
+evalChars list funTrans =
+    List.map funTrans list
+
+
+
+-- 4️⃣ headers component
+
+
+headers : String -> Html msg
+headers param =
+    div []
+        [ h1 [] [ text param ]
+        , h2 [] [ text param ]
+        , h3 [] [ text param ]
+        , h4 [] [ text param ]
+        , h5 [] [ text param ]
+        , h6 [] [ text param ]
+        ]
+
+
+
+-- 5️⃣ hyperlink component
+
+
+hyperlink : String -> String -> Html msg
+hyperlink url label =
+    a [ href url ] [ text label ]
